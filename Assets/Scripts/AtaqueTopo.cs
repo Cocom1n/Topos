1329ace8d04b7cbaae8732071590467ae9a5795a
Joaset <jtf1wft@gmail.com>
2024-07-05 04:01:03 -0300

    animacion topo

--- Assets/Scripts/AtaqueTopo.cs
+++ Assets/Scripts/AtaqueTopo.cs
@@ -5,16 +5,12 @@ using UnityEngine;
 public class AtaqueTopo : MonoBehaviour
 {
     public Animator animator;
-    private bool jugadorEnRango = false; // Para controlar si el jugador está en el área de ataque
-    private GameObject player; // Referencia al jugador
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            player = other.gameObject;
-            jugadorEnRango = true;
-            StartCoroutine(DelayedAttack());
+            animator.SetBool("ataqueTopo", true);
         }
     }
 
@@ -22,36 +18,7 @@ public class AtaqueTopo : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            jugadorEnRango = false;
+            animator.SetBool("ataqueTopo", false);
         }
     }
-
-    private IEnumerator DelayedAttack()
-    {
-        yield return new WaitForSeconds(0.5f); // Esperar 2 segundos
-        animator.SetBool("ataqueTopo", true);
-        if (jugadorEnRango && player != null) // Verificar si el jugador aún está en el área de ataque
-        {
-            player.GetComponent<Player>().quitarVida();
-        }
-    }
-/*
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            StartCoroutine(DelayedAttack(other.gameObject));
-        }
-    }
-
-    private IEnumerator DelayedAttack(GameObject player)
-    {
-        yield return new WaitForSeconds(1.5f); // Esperar 2 segundos
-        if (player != null) // Verificar si el jugador aún está en el área de ataque
-        {
-            animator.SetBool("ataqueTopo", true);
-            player.GetComponent<Player>().quitarVida();
-        }
-    }*/
-
 }
--- Assets/Scripts/MovTopo.cs
+++ Assets/Scripts/MovTopo.cs
@@ -7,13 +7,13 @@ public class MovTopo : MonoBehaviour
     [SerializeField] Transform[] puntos;
     [SerializeField] float velocidad;
     [SerializeField] bool espera;
+    private Animator animator;
 
     void Start()
     {
-        //velocidad = 4f;
+        animator = GetComponent<Animator>();
         StartCoroutine("MueveTopo");
-        espera = false;
-        //StartCoroutine("EnemigoCamina");
+        espera = false; 
     }
 
     private void Update()
@@ -34,8 +34,10 @@ public class MovTopo : MonoBehaviour
                 while (Vector2.Distance(transform.position, nuevaPosicion) > 0.001f)
                 {
                     transform.position = Vector2.MoveTowards(transform.position, nuevaPosicion, velocidad * Time.deltaTime);
+                    animator.SetBool("move", true);
                     yield return null;
                 }
+                animator.SetBool("move", false);
                 yield return new WaitForSeconds(1);
                 if (i < 1) i++; else i = 0;
                 transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
@@ -45,6 +47,7 @@ public class MovTopo : MonoBehaviour
     IEnumerator EnemigoCamina()
 {
     velocidad = 0;
+    //animator.SetBool("move", false);
     yield return new WaitForSeconds(2);
     espera = false;
     velocidad = 4;
